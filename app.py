b4524b8a5d5fc9a7712126ec520852e8e35ffd1d
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-22 22:16:19 +0530

    app.py

--- app.py
+++ app.py
@@ -142,6 +142,11 @@ def user_input(user_question, api_key):
     print('response is here......',response["output_text"])
     st.write("AI Buddy: ", response["output_text"])
 
+def gradient(color1, color2, color3, content1, content2):
+    st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:60px;border-radius:2%;">'
+                f'<span style="color:{color3};">{content1}</span><br>'
+                f'<span style="color:white;font-size:17px;">{content2}</span></h1>', 
+                unsafe_allow_html=True)
 
 
 def main():
