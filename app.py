9f09aa22b8400b404373dd3b47f044d638a82442
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-27 17:36:04 +0530

    app.py

--- app.py
+++ app.py
@@ -17,6 +17,61 @@ from streamlit_lottie import st_lottie
 import requests
 st.set_page_config(page_title="Pranav Baviskar ", layout="wide",page_icon='🧑🏻‍💼')
 
+
+st.markdown("""
+    <style>
+        @keyframes gradientAnimation {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+
+        .animated-gradient-text_ {
+            font-family: "Graphik Semibold";
+            font-size: 42px;
+            background: linear-gradient(45deg, rgb(245, 58, 126) 30%, rgb(200, 1, 200) 55%, rgb(197, 45, 243) 20%);
+            background-size: 300% 200%;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            animation: gradientAnimation 10s ease-in-out infinite;
+        }
+        @keyframes animate_ {
+            0%, 18%, 20%, 50.1%,60%, 65.1%, 80%,90.1%,92% {
+                color: #0e3742;
+                text-shadow: none;
+                }
+            18.1%, 20.1%, 30%,50%,60.1%,65%,80.1%,90%, 92.1%,100% {
+                color: #fff;
+                text-shadow: 0 0 10px rgb(197, 45, 243),
+                             0 0 20px rgb(197, 45, 243);
+                }
+            }
+        
+        .animated-gradient-text_ {
+                    font-family: "Graphik Semibold";
+                    font-size: 35px;
+                    color: #FFF;
+                    transition: color 0.5s, text-shadow 0.5s;
+                }
+
+        .animated-gradient-text_:hover {
+                    animation: animate_ 5s linear infinite;
+                }
+
+        
+    </style>
+    <p class="animated-gradient-text_">
+        Pranav Baviskar | Management Consultant @ Accenture Strategy
+    </p>
+""", unsafe_allow_html=True)
+
+
 particle_html = """
 <!DOCTYPE html>
 <html lang="en">
@@ -171,58 +226,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-st.markdown("""
-    <style>
-        @keyframes gradientAnimation {
-            0% {
-                background-position: 0% 50%;
-            }
-            50% {
-                background-position: 100% 50%;
-            }
-            100% {
-                background-position: 0% 50%;
-            }
-        }
-
-        .animated-gradient-text_ {
-            font-family: "Graphik Semibold";
-            font-size: 42px;
-            background: linear-gradient(45deg, rgb(245, 58, 126) 30%, rgb(200, 1, 200) 55%, rgb(197, 45, 243) 20%);
-            background-size: 300% 200%;
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-            animation: gradientAnimation 10s ease-in-out infinite;
-        }
-        @keyframes animate_ {
-            0%, 18%, 20%, 50.1%,60%, 65.1%, 80%,90.1%,92% {
-                color: #0e3742;
-                text-shadow: none;
-                }
-            18.1%, 20.1%, 30%,50%,60.1%,65%,80.1%,90%, 92.1%,100% {
-                color: #fff;
-                text-shadow: 0 0 10px rgb(197, 45, 243),
-                             0 0 20px rgb(197, 45, 243);
-                }
-            }
-        
-        .animated-gradient-text_ {
-                    font-family: "Graphik Semibold";
-                    font-size: 35px;
-                    color: #FFF;
-                    transition: color 0.5s, text-shadow 0.5s;
-                }
-
-        .animated-gradient-text_:hover {
-                    animation: animate_ 5s linear infinite;
-                }
-
-        
-    </style>
-    <p class="animated-gradient-text_">
-        Pranav Baviskar | Management Consultant @ Accenture Strategy
-    </p>
-""", unsafe_allow_html=True)
 st.write("Hey there, I'm Pranav! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. Check out my LinkedIn profile on https://www.linkedin.com/in/pranav-baviskar/")
 
 
