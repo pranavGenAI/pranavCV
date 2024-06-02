8f3cd84fa4de1690ea33b61724444e665b741bfa
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-06-02 10:55:31 +0530

    app.py

--- app.py
+++ app.py
@@ -191,6 +191,7 @@ def main():
                 .text-box{{
                     padding: 20px 30px;
                     background: rgba(0, 0, 0, 0.4); 
+		    color: rgba(255, 255, 255, 0.7);
                     position: relative;
                     border-radius: 6px;
                     font-size: 15px;
