17b8d35ca0a4cac3f4d6e227a69f0d50e1027aef
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-06-03 10:54:12 +0530

    app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown(video_html, unsafe_allow_html=True)
 
 
 # HTML for particles animation
-st.header('Pranav Baviskar CV | Management Consultant | AI Enthusiast')
+st.header(':blue[cool]: Pranav Baviskar CV | Management Consultant | AI Enthusiast')
 
 st.write("Hey there, I'm Pranav! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. Check out my LinkedIn profile on https://www.linkedin.com/in/pranav-baviskar/")
 
