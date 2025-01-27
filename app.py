67f5cea244a5a74846fdec70b9088b198373fbac
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-01-27 13:12:09 +0530

    app.py

--- app.py
+++ app.py
@@ -215,10 +215,10 @@ def main():
         if user_question:
             if st.button("Ask Question"):
                 user_input(user_question, api_key)
-
-    st.markdown("My Projects", unsafe_allow_html=True)    
     st.markdown("""""", unsafe_allow_html=True)    
+    st.markdown("""""", unsafe_allow_html=True)
     st.markdown("""""", unsafe_allow_html=True)    
+    st.subheader('🗂️ Personal Projects')
     st.markdown("""
         <div style="text-align: center;">
             <a href="https://pranavbaviskarcv.streamlit.app/">
