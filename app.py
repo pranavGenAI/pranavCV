5c4423a3cd68abef9e567926fa2edc7ea119efcc
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-03-17 10:07:51 +0530

    app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # This is the first API key input; no need to repeat it in the main function.
-api_key = "AIzaSyAPU11znVrGFm43zFebfwQ8ofllevmy83A"
+api_key = "AIzaSyCiPGxwD04JwxifewrYiqzufyd25VjKBkw"
 groq_api_key = "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"
 
 if 'responses' not in st.session_state:
